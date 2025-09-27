a140f50cdbdc7bd41fafeae294948b0b7725d17f
Ghaithzoaiter <ghaithzezotiktok@gmail.com>
2025-09-27 16:00:22 +0300

    Initial commit

---
+++ src/config/config.js
@@ -0,0 +1,10 @@
+const config = {
+  development: {
+    backendUrl: "https://clothes-shop3d.onrender.com/api/v1/dalle",
+  },
+  production: {
+    backendUrl: "https://clothes-shop3d.onrender.com/api/v1/dalle",
+  },
+};
+
+export default config;
